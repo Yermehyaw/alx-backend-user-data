61bdb00f3057f4f676e95338bf242fead3567e15
Yermehyaw <yermehyaw@gmail.com>
2024-11-08 04:32:43 +0000

    Obfuscate user dat from log

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python3
+"""
+Logs, reads and filter user sensitive/non-sesnsitive data
+
+Modules imported: typing, re
+
+"""
+from typing import List
+from re import sub
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """Returns an obfuscated log message"""
+    for field in fields:
+        pattern = rf'{field}=\s*(.*?){separator}'
+        message = sub(pattern, f'{field}={redaction}{separator}', message)
+    return message
