123e6034b66ec9d670958f61531d230cb97942c0
Yermehyaw <yermehyaw@gmail.com>
2024-11-08 04:45:03 +0000

    Reduce no of lines even more

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,12 +9,7 @@ from typing import List
 from re import sub
 
 
-def filter_datum(
-        fields: List[str],
-        redaction: str,
-        message: str,
-        separator: str
-) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Returns an obfuscated log message"""
     pattern = rf'({"|".join(fields)})=\s*(.*?){separator}'
     return sub(pattern, rf'\1={redaction}{separator}', message)
