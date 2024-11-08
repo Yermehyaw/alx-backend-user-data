4d963642e83c6c5a07f85cc47d10df6c8eb9d95f
Yermehyaw <yermehyaw@gmail.com>
2024-11-08 04:49:56 +0000

    Fix pycodestyle errs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """
 Logs, reads and filter user sensitive/non-sesnsitive data
@@ -19,5 +18,5 @@ def filter_datum(
     """Returns an obfuscated log message"""
     for field in fields:
         pattern = rf'{field}=\s*(.*?){separator}'
-        message = re.sub(pattern, field + '=' + redaction + separator, message)
+        message = re.sub(pattern, f'{field}={redaction}{separator}', message)
     return message
