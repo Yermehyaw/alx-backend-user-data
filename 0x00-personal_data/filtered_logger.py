134a263a8997b8d032ddef1a0602f44567935df4
Yermehyaw <yermehyaw@gmail.com>
2024-11-10 02:20:27 +0000

    Fix typo

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -47,7 +47,7 @@ class RedactingFormatter(logging.Formatter):
         )
 
 
-PII_FIELDS = ("email", "ssn", "password", "ip", "user-agent")
+PII_FIELDS = ("email", "ssn", "password", "ip", "user_agent")
 
 
 def get_logger() -> logging.Logger:
