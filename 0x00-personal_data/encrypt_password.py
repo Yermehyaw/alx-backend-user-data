a8de7010ae939d2f406e550f6b0d45ad285e6450
Yermehyaw <yermehyaw@gmail.com>
2024-11-10 03:25:45 +0000

    Hash and validare passwords with bcrypt

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Encrypts and validares user password
+
+Modukes imported: bcrypt
+
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns a hashed pasword"""
+    if not isinstance(password, str):
+        raise TypeError
+        return
+
+    password = password.encode('utf-8')
+
+    hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates password matches a hash"""
+    return bcrypt.checkpw(password, hashed_password)
