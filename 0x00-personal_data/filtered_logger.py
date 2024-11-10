7687a54fca24475ae0611500ed5693422863660e
Yermehyaw <yermehyaw@gmail.com>
2024-11-10 00:55:30 +0000

    Implement a format mthd

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,11 +2,15 @@
 """
 Logs, reads and filter user sensitive/non-sesnsitive data
 
-Modules imported: typing, re
+Modules imported: typing, re, logging
 
 """
-from typing import List
+from typing import (
+    List,
+    Sequence,
+)
 import re
+import logging
 
 
 def filter_datum(
@@ -20,3 +24,24 @@ def filter_datum(
         pattern = rf'{field}=\s*(.*?){separator}'
         message = re.sub(pattern, f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Sequence = (, )) -> None:
+        """Object initializer"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats the logging for class objects"""
+        return filter_datum(
+            self.fields, self.REDACTION,
+            record.getMessage(), self.SEPARATOR
+        )
