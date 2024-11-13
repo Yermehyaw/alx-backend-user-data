5075286d7834fb20e94bf537dd7d80a34129b0d1
Yermehyaw <yermehyaw@gmail.com>
2024-11-13 03:19:40 +0000

    Create a BasicAuth cls for Basic Authentication

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Implements BasicAuth in API
+
+Modules imported:
+
+"""
+from api.v1.auth.auth import Auth
+from os import getenv
+
+
+class BasicAuth(Auth):
+    """Holds mthds and variables for BasicAuthentication in the API
+    """
+    pass
