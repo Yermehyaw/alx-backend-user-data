55598307691534b773ce2ec85b218af5c93ad0fb
Yermehyaw <yermehyaw@gmail.com>
2024-11-13 03:32:39 +0000

    Retrieve Base64 encoded str

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,10 +6,19 @@ Modules imported:
 
 """
 from api.v1.auth.auth import Auth
+frpm flask import request
 from os import getenv
 
 
 class BasicAuth(Auth):
     """Holds mthds and variables for BasicAuthentication in the API
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Returns the Base64 encoded str in Authorization header"""
+        if not isinstance(authorization_header, str):
+            return None
+
+        encoded = authorization_header.split()
+        if encoded[0] != 'Basic':
+            return None
+        return encoded[1]
