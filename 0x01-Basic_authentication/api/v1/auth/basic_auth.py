dd7ba357da93b5e9fc7f6be3adf64d11ebd7faf9
Yermehyaw <yermehyaw@gmail.com>
2024-11-13 03:48:09 +0000

    Decode basic64 strings

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,14 +6,17 @@ Modules imported:
 
 """
 from api.v1.auth.auth import Auth
-frpm flask import request
+import base64
+from binascii import Error
 from os import getenv
 
 
 class BasicAuth(Auth):
     """Holds mthds and variables for BasicAuthentication in the API
     """
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str
+    ) -> str:
         """Returns the Base64 encoded str in Authorization header"""
         if not isinstance(authorization_header, str):
             return None
@@ -22,3 +25,18 @@ class BasicAuth(Auth):
         if encoded[0] != 'Basic':
             return None
         return encoded[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str
+    ) -> str:
+        """Returns the decoded Base64 str"""
+        if not isinstance(base64_authorization, str):
+            return None
+
+        try:
+            decoded = base64.b64decode(validate=True)
+            decoded = decoded.decode('utf-8')
+        except (Error, UnicodeDecodeError):
+            return None
+
+        return decoded
