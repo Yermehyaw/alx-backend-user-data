edcac7588b5e2ec44422acb6fb83710a7a039754
Yermehyaw <yermehyaw@gmail.com>
2024-11-21 23:51:16 +0000

    Remove mthd frm cls anf make exec

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,19 +8,9 @@ Modules imported:
 import bcrypt
 
 
-class Auth():
-    """Authenticates user credentials to enable user log-in
-
-    Attributes:
-    None
-
-    """
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hashes the users password credential"""
-
-        if not isinstance(password, str):
+def _hash_password(password: str) -> bytes:
+    """Hashes the users password credential"""
+    if not isinstance(password, str):
             return
-
-        salt = bcrypt.gensalt()
-        return bcrypt.hashpw(password.encode('utf-8'), salt)
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode('utf-8'), salt)
